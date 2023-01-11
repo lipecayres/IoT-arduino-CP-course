165b2689a02f489ee7a92eda4c88f28697400fe7
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-01-10 21:20:01 -0500

    functions

--- flashing-lights-ass1.c
+++ flashing-lights-ass1.c
@@ -15,3 +15,64 @@ int dashDelay = 1000;
 int spaceLettersDelay = 3*dotDelay; // three units
 int spaceWordsDelay = 7 * dotDelay; // seven units
 
+// Prototypes
+
+void fastBlinkBlueLED (int times);
+void slowBlinkBlueLED (int times);
+void fastBlinkRedLED (int times);
+void slowBlinkRedLED (int times);
+
+
+
+
+// Funtions
+
+	// BLINK BLUE LED
+
+void fastBlinkBlueLED (int times) // Blink Blue LED FAST
+{
+  for (int i = 0; i <times; i++) {
+    digitalWrite(blueLED, HIGH);
+    delay(dotDelay);
+    digitalWrite(blueLED, LOW);
+    delay(dotDelay);
+  }
+    return;
+}
+
+
+void slowBlinkBlueLED (int times) // Blink blue LED SLOW
+{
+  for (int i = 0; i <times; i++) {
+    digitalWrite(blueLED, HIGH);
+    delay(dashDelay); 
+    digitalWrite(blueLED, LOW);
+    delay(dashDelay);   
+  }
+  return;
+}
+
+	// RED LED
+
+void fastBlinkRedLED (int times) // Blink Red LED FAST
+{
+  for (int i = 0; i < times; i++) {
+    digitalWrite(redLED, HIGH);
+    delay(dotDelay); 
+    digitalWrite(redLED, LOW);
+    delay(dotDelay);   
+    }
+
+  return; 
+}
+
+void slowBlinkRedLED (int times) // Blink red LED SLOW
+{
+  for (int i = 0; i < times; i++) {
+    digitalWrite(redLED, HIGH);
+    delay(dashDelay); 
+    digitalWrite(redLED, LOW);
+    delay(dashDelay); 
+  }
+    return;
+}
