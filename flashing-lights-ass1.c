59f8d73158d3e854156f5386eb59d92df4cb4cb5
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-01-10 21:18:51 -0500

    morse code

--- flashing-lights-ass1.c
+++ flashing-lights-ass1.c
@@ -8,3 +8,10 @@
 	// LED indication
 int blueLED = 13; 
 int redLED = 11;
+
+	// Morse code pattern time(mS)
+int dotDelay = 100; 
+int dashDelay = 1000;
+int spaceLettersDelay = 3*dotDelay; // three units
+int spaceWordsDelay = 7 * dotDelay; // seven units
+
