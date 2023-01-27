5eda3dc07d8454f2764ca8ad4a4bbf7a5f5452a5
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-01-26 21:28:34 -0500

    variables

---
+++ 3week/ass3.c
@@ -0,0 +1,15 @@
+//
+// Assignment 3
+// 200548559 - Felipe Simoes Cayres
+// Date:
+// Description:
+//
+
+//Variables
+
+int rotationPin = A0; //Rotation sensor plugged into pin A0
+int data=0; //Used to store data from sensor.
+
+int RGBRedPin = 9; //The red RGB LED is connected pin 9 of the Arduino.
+int RGBGreenPin = 10; //The green RGB LED is connected pin 10 of the Arduino.
+int RGBBluePin = 11; //The blue RGB LED is connected pin 11 of the Arduino.
