c49686efb7a1708cf92b8a7470b40ba788076af4
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-01-31 16:03:18 -0500

    variables

---
+++ 4week/ass4.c
@@ -0,0 +1,14 @@
+//
+// 200548559 - Felipe Simoes Cayres
+// Date: 
+// Description:  Assignment 4 - COMP 1045
+//
+
+//Variables
+	
+  int photoResistorPin = A1; //Photoresistor sensor plugged into pin A0
+  int RGBRedPin = 12; //The red RGB LED is connected pin 12 of the Arduino.
+  int RGBGreenPin = 8; //The green RGB LED is connected pin 8 of the Arduino.
+  int RGBBluePin = 13; //The blue RGB LED is connected pin 13 of the Arduino.
+  int RGBYellowPin = 7; //The blue RGB LED is connected pin 7 of the Arduino.
+  int binaryNumber;     // Variable to store binary number
