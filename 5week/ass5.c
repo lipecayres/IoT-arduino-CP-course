65b328513529a23acceacc233c410dcfcf6b863d
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-02-10 17:03:28 -0500

    variables

--- 5week/ass5.c
+++ 5week/ass5.c
@@ -1,96 +1,20 @@
 //
-// Assignment 5 - Question 1 and 2
+// Assignment 5 - Level 3 and 4
 // 200548559 - Felipe Simoes Cayres
 //
 
 // Prototypes
 
+  void turnOffRGBLED ();  								// Turn off RGB LED
+  void turnOnLEDByRGB (int red, int green, int blue);  	// Turn On led using RGB CODE
 
+  void ciclyngRGBColors(int data);  					// Turn On ciclying colors on RGB LED  
+  void redLedON(); 										// Turn on Red LED
+  void greenLedON(); 									// Turn on Green LED
 
-// Variables
+  void clickButton1();  								// Change button 1 state
+  void clickButton2(); 									// Change button 2 state
 
-  int redLedPin = 13;
-  int greenLedPin = 12; //The blue LED is connected pin 13 of the Arduino.
+  void level3ClickButton1(); 							// Change state to true
+  void level3ClickButton2(); 							// Change state to false
 
-  int RGBRedPin = 9; //The red RGB LED is connected pin 9 of the Arduino.
-  int RGBGreenPin = 10; //The green RGB LED is connected pin 10 of the Arduino.
-  int RGBBluePin = 11; //The blue RGB LED is connected pin 11 of the Arduino.
-
-
-  int button1Pin = 2; //The SW1 button is connect to pin 2 of the Arduino.
-  int button2Pin = 3;
-
-  volatile bool state = true;
-  volatile bool state2 = false;
-
-// Setup
-
-void setup() { //The Setup function runs once
-
-  pinMode(greenLedPin, OUTPUT); //Setup blue LED pin as an output pin.
-  pinMode(redLedPin, OUTPUT); //Setup button1 pin as an input pin.
-  attachInterrupt(digitalPinToInterrupt(button1Pin), clickButton1, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(button2Pin), clickButton2, CHANGE);
-  Serial.begin(9600);
-}
-
-
-			//
-			// Level 3  - cycling through three RGB colors
-			//
-
-void loop() { //The loop function runs forever.
-
-  while (state == true){
-    int data = 500;
-    if (state == true) { //Check to see if button1 is pressed.
-        // Turn on RED LED
-      turnOnLEDByRGB (255,0,0); // red (255, 0, 0);
-      delay(data);
-      }
-
-    // Turn on Green LED
-    if (state == true) {
-      turnOnLEDByRGB (0,255,0); // green (0, 255, 0)
-      delay(data);
-    }
-
-    // Turn on Blue LED
-    if (state == true){
-      turnOnLEDByRGB (0,0,255); // blue (0, 0, 255)
-      delay(data); 
-    }
-  }
-
-  while (state ==false) {
-	turnOffRGBLED();
-  }
-  
-}
-
-	// Functions
-
-void clickButton1() {
-	state = true;
-
-}
-
-void clickButton2() {
-	state = false;
-}
-
-
-
-// Turn On led using RGB CODE
-void turnOnLEDByRGB (int red, int green, int blue){
-  analogWrite(RGBRedPin, red); //Turn on RED
-  analogWrite(RGBGreenPin, green); //Turn on GREEN
-  analogWrite(RGBBluePin, blue); //Turn on BLUE
-}
-
-// Turn off LED
-void turnOffRGBLED () {
-  analogWrite(RGBRedPin, LOW); //Turn off RED
-  analogWrite(RGBBluePin, LOW); //Turn off BLUE
-  analogWrite(RGBGreenPin, LOW); //Turn off GREEN
-}
