e950b0245973a439f0298be3b7bc6582f48afee0
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-03-20 21:18:03 -0400

    ass8 default code

---
+++ 8week/defaultCode.ino
@@ -0,0 +1,152 @@
+#include <IRremote.hpp>
+
+#define blueLed 7
+
+
+void setup()
+{
+  Serial.begin(9600);
+  IrReceiver.begin(11, ENABLE_LED_FEEDBACK); // Start the receiver
+}
+
+void loop() {
+
+  if (IrReceiver.decode()) {
+    Serial.println(IrReceiver.decodedIRData.decodedRawData); // Print "old" raw data
+
+    IrReceiver.resume(); // Enable receiving of the next value
+
+
+    switch (IrReceiver.decodedIRData.decodedRawData) {
+      case 4278238976: // power
+
+      digitalWrite(blueLed,HIGH);
+
+      break;
+      case 4261527296:  // vol+
+
+      digitalWrite(blueLed,LOW);
+
+      break;
+      case 4244815616:  // func/Stop
+
+      // insert code here
+		Serial.println("Felipe");
+      break; 
+      case 4211392256: // backward
+
+      // insert code here
+
+      break;
+      case 4194680576:  // play/pause
+
+      // insert code here
+
+
+      break;
+      case 4177968896:  // forward
+
+      // insert code here
+
+
+      break;
+      case 4144545536:  // down
+
+      // insert code here
+
+
+      break;
+      case 4127833856:  // vol-
+
+      // insert code here
+
+
+      break;
+      case 4111122176:  // up
+
+      // insert code here
+
+
+      break;
+      case 4077698816:  // number 0
+
+      // insert code here
+
+
+      break;
+      case 4060987136:  // EQ
+
+      // insert code here
+
+
+      break;
+      case 4044275456:  // ST/REPT
+
+      // insert code here
+
+
+      break;
+      case 4010852096:  // number 1
+
+      // insert code here
+
+
+      break;
+      case 3994140416:  // number 2
+
+      // insert code here
+
+
+      break;
+      case 3977428736:  // number 3
+
+      // insert code here
+
+
+      break;
+      case 3944005376:  // number 4
+
+      // insert code here
+
+
+      break;
+      case 3927293696:  // number 5
+
+      // insert code here
+
+
+      break;
+      case 3910582016:  // number 6
+
+      // insert code here
+
+
+      break;
+      case 3877158656:  // number 7
+
+      // insert code here
+
+
+      break;
+      case 3860446976:  // number 8
+
+      // insert code here
+
+
+      break;
+      case 3843735296:  // number 9 
+
+      // insert code here
+
+
+      break; 
+      default:
+      Serial.print("Error");
+      break;
+
+    }
+
+  }
+
+}
+
