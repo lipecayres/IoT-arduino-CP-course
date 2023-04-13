82da675f34298686339a2e056f9a6d04378bedc5
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-04-13 10:14:29 -0400

    safe locker system v1 updated

--- safe-lock-system/safe-locker.ino
+++ safe-lock-system/safe-locker.ino
@@ -2,10 +2,10 @@
 #include <LiquidCrystal.h>
 #include <Servo.h>
 
-#define buzz 11  		// piezo
-#define servo 10		// servo
-#define redLed 12		// red led
-#define greenLed 13		// green led
+#define buzz 11     // piezo
+#define servo 10    // servo
+#define redLed 12   // red led
+#define greenLed 13 // green led
 
 // lcd settings
 LiquidCrystal lcd(A0, A1, A2, A3, A4, A5);
@@ -19,18 +19,18 @@ const byte ROWS = 4;
 const byte COLS = 4;
 
 char hexakey[ROWS][COLS] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}};
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}};
 
 byte rowPins[ROWS] = {2, 3, 4, 5};
 byte colPins[COLS] = {6, 7, 8, 9};
 Keypad keypad = Keypad(makeKeymap(hexakey), rowPins, colPins, ROWS, COLS);
 
 // passwords and users
-String password[5] = {"AD00","1020","3040","5060","7080"};
-String users[5] = {"admin","A0012","B0005","C0037","D0010"};
+String password[5] = {"AD00", "1020", "3040", "5060", "7080"};
+String users[5] = {"admin", "A0012", "B0005", "C0037", "D0010"};
 String inputKey = "";
 
 // variables
@@ -56,7 +56,7 @@ void loop()
   //
   // get complete attempt password from user
   //
-  
+
   // get entry of keyboard
   char key = keypad.getKey();
 
@@ -69,64 +69,79 @@ void loop()
     lcd.print("  SAFE  LOCKER  ");
     lcd.setCursor(6, 1);
 
-    //display * (password simbol) to each pressed key
+    // display * (password simbol) to each pressed key
     for (int i = 0; i <= position; i++)
     {
       lcd.print("*");
       playKeySound(1);
     }
-	// saving pressed btn and going to next position 
+    // saving pressed btn and going to next position
     inputKey += key;
     position++;
-
   }
-  
+
   //
   // waiting for 4 pressed btn's to check password
   //
-  
+
   while (position >= 4)
   {
 
-    //check each stored password once
-    for (int j=0; j<5;j++){
-      for(int i = 0; i<4; i++){
+    // check each stored password once
+    for (int j = 0; j < 5; j++)
+    {
+      for (int i = 0; i < 4; i++)
+      {
 
         // char check
-        if (inputKey[i] == password[j][i]){
-		  
+        if (inputKey[i] == password[j][i])
+        {
+
           // add 1 to each valid char on password
           validEntry++;
-          
-          //track username at this time
+
+          // track username at this time
           usernamePosition = j;
         }
 
-        // stop loop
-        if(validEntry ==4){
+        // check if password is valid
+        if (validEntry == 4)
+        {
+          // stop loop
           i = 100;
-        } else {
-          validEntry =0;
+        }
+        // if password is invalid after check
+        else if (validEntry != 4 && i == 3)
+        {
+          // new attempt (back to zero)
+          validEntry = 0;
         }
       }
     }
 
-    // check if password ok
+    //
+    // return message after check password
+    //
+
+    // password valid
     if (validEntry == 4)
     {
       // unlock device
+      Serial.println("System OPEN");
       digitalWrite(greenLed, HIGH);
       unlockdoor();
       digitalWrite(greenLed, LOW);
 
       // reset variables to next attempt
       position = 0;
-      validEntry =0;
+      validEntry = 0;
       inputKey = "";
-
-    } else {
-
-      // failed message 
+    }
+    // password not valid
+    else
+    {
+      Serial.println("Password INVALID");
+      // failed message
       attempts--;
       digitalWrite(redLed, HIGH);
 
@@ -134,13 +149,12 @@ void loop()
       digitalWrite(redLed, LOW);
       // reset variables to next attempt
       position = 0;
-      validEntry =0;
+      validEntry = 0;
       inputKey = "";
     }
     // password default message
     displayDefaultMessage();
   }
-
 }
 
 // functions:
@@ -154,13 +168,11 @@ void unlockdoor()
   lcd.print("     WELCOME    ");
   lcd.setCursor(6, 1);
   lcd.print(users[usernamePosition]);
-  Serial.println(users[usernamePosition]);
 
   playKeySound(3);
 
-
   // servo position to open device
-  for (pos = 90; pos >= 0; pos -= 5) 
+  for (pos = 90; pos >= 0; pos -= 5)
   {
     myservo.write(pos);
     delay(5);
@@ -172,8 +184,8 @@ void unlockdoor()
   lockDevice();
 
   // servo position to close device
-  for (pos = 0; pos <= 90; pos += 5) 
-  {                                   
+  for (pos = 0; pos <= 90; pos += 5)
+  {
     myservo.write(pos);
     delay(15);
 
@@ -206,7 +218,6 @@ void failedAttempt()
   delay(500);
 }
 
-
 // default message on LCD
 void displayDefaultMessage()
 {
@@ -219,15 +230,16 @@ void displayDefaultMessage()
 // play sound to key press
 void playKeySound(int ocurrences)
 {
-  for (int i = 0;i<ocurrences;i++){
+  for (int i = 0; i < ocurrences; i++)
+  {
     digitalWrite(buzz, HIGH);
     delay(100);
     digitalWrite(buzz, LOW);
-    delay(100);  
+    delay(100);
   }
 }
 
-// lock device after some time opened 
+// lock device after some time opened
 void lockDevice()
 {
   // regressive counter lock message (upper line)
@@ -236,12 +248,12 @@ void lockDevice()
   lcd.println("   LOCKING IN   ");
 
   // counter lock time -> 5sec (lower line)
-  for (int i=5; i>0;i--){
+  for (int i = 5; i > 0; i--)
+  {
     lcd.setCursor(7, 1);
     lcd.print(i);
     playKeySound(1);
     delay(1000);
-
   }
   // play wrong attempt sound (4 times key press)
   playKeySound(4);
@@ -252,12 +264,12 @@ void lockDevice()
   lcd.print("   RE-LOCKING   ");
 
   // display "dot" counter and key sound (lower line)
-  for (int i=0; i<4;i++){
-    delay(200);
-    Serial.print(".");
-    lcd.setCursor(6+i, 1);
+  for (int i = 0; i < 4; i++)
+  {
+    lcd.setCursor(6 + i, 1);
     lcd.print(".");
     delay(200);
     digitalWrite(buzz, LOW);
   }
+  Serial.println("System CLOSED");
 }
